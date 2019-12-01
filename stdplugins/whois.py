c5768c70185f99bb88b087ea6908f5fd70ad31b9
HafizZiq <hafizuddinismail552@gmail.com>
2019-12-01 10:30:28 +0800

    update whois.py

--- stdplugins/whois.py
+++ stdplugins/whois.py
@@ -36,6 +36,11 @@ async def _(event):
     if first_name is not None:
         # some weird people (like me) have more than 4096 characters in their names
         first_name = first_name.replace("\u2060", "")
+    last_name = replied_user.user.last_name
+    # last_name is not Manadatory in @Telegram
+    if last_name is not None:
+        last_name = html.escape(last_name)
+        last_name = last_name.replace("\u2060", "")
     # inspired by https://telegram.dog/afsaI181
     user_bio = replied_user.about
     if user_bio is not None:
@@ -47,8 +52,10 @@ async def _(event):
         dc_id = "Need a Profile Picture to check **this**"
         location = str(e)
     caption = """Detailed Infos:
+    	
 ID🔖: <code>{}</code>
-Name🤵: <a href='tg://user?id={}'>{}</a>
+First Name🤵: <a href='tg://user?id={}'>{}</a>
+Last Name🤵: {}
 Bio✍️: {}
 Data Centre Number🌏: {}
 Number of Profile Pics🖼: {}
@@ -60,6 +67,7 @@ Groups in Common👥: {}
         user_id,
         user_id,
         first_name,
+        last_name,
         user_bio,
         dc_id,
         replied_user_profile_photos_count,
